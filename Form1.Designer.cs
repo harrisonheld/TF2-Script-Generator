28f7b3d87bcc038eae10aab9fcd73476973f9cd1
harrisonheld <harrydheld@gmail.com>
2021-01-26 01:15:12 -0600

    Add files via upload
    Added all the files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace TF2SpamScriptGen
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.linesInputTextBox = new System.Windows.Forms.TextBox();
+            this.scriptsFolderButton = new System.Windows.Forms.Button();
+            this.scriptsFolderTextBox = new System.Windows.Forms.TextBox();
+            this.waitTimeInput = new System.Windows.Forms.NumericUpDown();
+            this.keyToBindTextbox = new System.Windows.Forms.TextBox();
+            this.waitTimeLabel = new System.Windows.Forms.Label();
+            this.keyToBindLabel = new System.Windows.Forms.Label();
+            this.generateButton = new System.Windows.Forms.Button();
+            this.saveSettingsButton = new System.Windows.Forms.Button();
+            this.loadButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.waitTimeInput)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // linesInputTextBox
+            // 
+            this.linesInputTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.linesInputTextBox.Location = new System.Drawing.Point(12, 69);
+            this.linesInputTextBox.Multiline = true;
+            this.linesInputTextBox.Name = "linesInputTextBox";
+            this.linesInputTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.linesInputTextBox.Size = new System.Drawing.Size(760, 486);
+            this.linesInputTextBox.TabIndex = 0;
+            // 
+            // scriptsFolderButton
+            // 
+            this.scriptsFolderButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.scriptsFolderButton.Location = new System.Drawing.Point(631, 9);
+            this.scriptsFolderButton.Name = "scriptsFolderButton";
+            this.scriptsFolderButton.Size = new System.Drawing.Size(141, 20);
+            this.scriptsFolderButton.TabIndex = 1;
+            this.scriptsFolderButton.Text = "Choose Scripts Folder";
+            this.scriptsFolderButton.UseVisualStyleBackColor = true;
+            this.scriptsFolderButton.Click += new System.EventHandler(this.scriptsFolderButton_Click);
+            // 
+            // scriptsFolderTextBox
+            // 
+            this.scriptsFolderTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.scriptsFolderTextBox.Location = new System.Drawing.Point(12, 9);
+            this.scriptsFolderTextBox.Name = "scriptsFolderTextBox";
+            this.scriptsFolderTextBox.ReadOnly = true;
+            this.scriptsFolderTextBox.Size = new System.Drawing.Size(613, 20);
+            this.scriptsFolderTextBox.TabIndex = 3;
+            // 
+            // waitTimeInput
+            // 
+            this.waitTimeInput.Location = new System.Drawing.Point(131, 38);
+            this.waitTimeInput.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+            this.waitTimeInput.Name = "waitTimeInput";
+            this.waitTimeInput.Size = new System.Drawing.Size(75, 20);
+            this.waitTimeInput.TabIndex = 4;
+            // 
+            // keyToBindTextbox
+            // 
+            this.keyToBindTextbox.Location = new System.Drawing.Point(286, 37);
+            this.keyToBindTextbox.Name = "keyToBindTextbox";
+            this.keyToBindTextbox.Size = new System.Drawing.Size(75, 20);
+            this.keyToBindTextbox.TabIndex = 5;
+            // 
+            // waitTimeLabel
+            // 
+            this.waitTimeLabel.AutoSize = true;
+            this.waitTimeLabel.Location = new System.Drawing.Point(12, 41);
+            this.waitTimeLabel.Name = "waitTimeLabel";
+            this.waitTimeLabel.Size = new System.Drawing.Size(113, 13);
+            this.waitTimeLabel.TabIndex = 6;
+            this.waitTimeLabel.Text = "Wait Time (In Frames):";
+            // 
+            // keyToBindLabel
+            // 
+            this.keyToBindLabel.AutoSize = true;
+            this.keyToBindLabel.Location = new System.Drawing.Point(212, 41);
+            this.keyToBindLabel.Name = "keyToBindLabel";
+            this.keyToBindLabel.Size = new System.Drawing.Size(68, 13);
+            this.keyToBindLabel.TabIndex = 7;
+            this.keyToBindLabel.Text = "Key To Bind:";
+            // 
+            // generateButton
+            // 
+            this.generateButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.generateButton.Location = new System.Drawing.Point(672, 561);
+            this.generateButton.Name = "generateButton";
+            this.generateButton.Size = new System.Drawing.Size(100, 28);
+            this.generateButton.TabIndex = 8;
+            this.generateButton.Text = "Save Script";
+            this.generateButton.UseVisualStyleBackColor = true;
+            // 
+            // saveSettingsButton
+            // 
+            this.saveSettingsButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.saveSettingsButton.Location = new System.Drawing.Point(631, 35);
+            this.saveSettingsButton.Name = "saveSettingsButton";
+            this.saveSettingsButton.Size = new System.Drawing.Size(141, 23);
+            this.saveSettingsButton.TabIndex = 11;
+            this.saveSettingsButton.Text = "Save Settings";
+            this.saveSettingsButton.UseVisualStyleBackColor = true;
+            // 
+            // loadButton
+            // 
+            this.loadButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.loadButton.Location = new System.Drawing.Point(12, 561);
+            this.loadButton.Name = "loadButton";
+            this.loadButton.Size = new System.Drawing.Size(100, 28);
+            this.loadButton.TabIndex = 12;
+            this.loadButton.Text = "Load Script";
+            this.loadButton.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 601);
+            this.Controls.Add(this.loadButton);
+            this.Controls.Add(this.saveSettingsButton);
+            this.Controls.Add(this.generateButton);
+            this.Controls.Add(this.keyToBindLabel);
+            this.Controls.Add(this.waitTimeLabel);
+            this.Controls.Add(this.keyToBindTextbox);
+            this.Controls.Add(this.waitTimeInput);
+            this.Controls.Add(this.scriptsFolderTextBox);
+            this.Controls.Add(this.scriptsFolderButton);
+            this.Controls.Add(this.linesInputTextBox);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "TF2 Spam Script Generator";
+            ((System.ComponentModel.ISupportInitialize)(this.waitTimeInput)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox linesInputTextBox;
+        private System.Windows.Forms.Button scriptsFolderButton;
+        private System.Windows.Forms.TextBox scriptsFolderTextBox;
+        private System.Windows.Forms.NumericUpDown waitTimeInput;
+        private System.Windows.Forms.TextBox keyToBindTextbox;
+        private System.Windows.Forms.Label waitTimeLabel;
+        private System.Windows.Forms.Label keyToBindLabel;
+        private System.Windows.Forms.Button generateButton;
+        private System.Windows.Forms.Button saveSettingsButton;
+        private System.Windows.Forms.Button loadButton;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+using System.Configuration;
+using Microsoft.WindowsAPICodePack.Dialogs;
+
+namespace TF2SpamScriptGen
+{
+    static class Program
+    {
+        const int defaultWaitTime = 200; //in case there are non-numeric characters in waitTime
+
+        static string scriptsFolder;
+        static string keyToBind;
+        static string waitTime;
+
+        static TextBox linesInputTextBox;
+        static TextBox scriptsFolderTextBox;
+        static Button scriptsFolderButton;
+        static NumericUpDown waitTimeInput;
+        static TextBox keyToBindTextBox;
+        static Button generateButton;
+        static Button saveSettingsButton;
+        static Button loadButton;
+
+        [STAThread]
+        static void Main(string[] args)
+        {
+            scriptsFolder = ConfigurationManager.AppSettings.Get("scriptsFolder");
+            keyToBind = ConfigurationManager.AppSettings.Get("keyToBind");
+            waitTime = ConfigurationManager.AppSettings.Get("waitTime");
+
+            Form1 form1 = new Form1();
+
+            linesInputTextBox = (TextBox)form1.Controls.Find("linesInputTextBox", false)[0];
+            scriptsFolderTextBox = (TextBox)form1.Controls.Find("scriptsFolderTextBox", false)[0];
+            scriptsFolderButton = (Button)form1.Controls.Find("scriptsFolderButton", false)[0];
+            waitTimeInput = (NumericUpDown)form1.Controls.Find("waitTimeInput", false)[0];
+            keyToBindTextBox = (TextBox)form1.Controls.Find("keyToBindTextBox", false)[0];
+            generateButton = (Button)form1.Controls.Find("generateButton", false)[0];
+            saveSettingsButton = (Button)form1.Controls.Find("saveSettingsButton", false)[0];
+            loadButton = (Button)form1.Controls.Find("loadButton", false)[0];
+
+            try
+            {
+                waitTimeInput.Value = int.Parse(waitTime);
+            }
+            catch
+            {
+                waitTimeInput.Value = defaultWaitTime;
+                waitTime = defaultWaitTime.ToString();
+            }
+
+            scriptsFolderTextBox.Text = scriptsFolder;
+            keyToBindTextBox.Text = keyToBind;
+
+            scriptsFolderButton.Click += new EventHandler(OnScriptsFolderButtonClick);
+            generateButton.Click += new EventHandler(OnGenerateButtonClick);
+            saveSettingsButton.Click += new EventHandler(OnSaveSettingsButtonClick);
+            loadButton.Click += new EventHandler(OnLoadButtonClick);
+            linesInputTextBox.KeyDown += new KeyEventHandler(OnLinesInputTextBoxKeyDown);
+
+            Application.Run(form1);
+        }
+        static void OnLinesInputTextBoxKeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.Modifiers.HasFlag(Keys.Control) && e.KeyCode == Keys.A)
+            {
+                e.SuppressKeyPress = true;
+                linesInputTextBox.SelectAll();
+            }
+        }
+        static void OnScriptsFolderButtonClick(object sender, EventArgs e)
+        {
+            CommonOpenFileDialog dialog = new CommonOpenFileDialog();
+            dialog.InitialDirectory = scriptsFolder;
+            dialog.IsFolderPicker = true;
+
+            if (dialog.ShowDialog() == CommonFileDialogResult.Ok)
+            {
+                dialog.InitialDirectory = dialog.FileName;
+                scriptsFolderTextBox.Text = dialog.FileName;
+            }
+        }
+        static void OnSaveSettingsButtonClick(object sender, EventArgs e)
+        {
+            UpdateSettingsVars();
+            ConfigurationManager.AppSettings.Set("scriptsFolder", scriptsFolder);
+            ConfigurationManager.AppSettings.Set("keyToBind", keyToBind);
+            ConfigurationManager.AppSettings.Set("waitTime", waitTime);
+        }
+        static void OnGenerateButtonClick(object sender, EventArgs e)
+        {
+            CommonSaveFileDialog dialog = new CommonSaveFileDialog();
+            dialog.InitialDirectory = scriptsFolder;
+            dialog.DefaultExtension = "cfg";
+
+            if (dialog.ShowDialog() == CommonFileDialogResult.Ok)
+            {
+                UpdateSettingsVars();
+                string[] lines = linesInputTextBox.Lines;
+                string[] scriptLines = GenerateScriptLines(lines);
+
+                File.WriteAllLines(dialog.FileName, scriptLines);
+            }
+        }
+        static void OnLoadButtonClick(object sender, EventArgs e)
+        {
+            CommonOpenFileDialog dialog = new CommonOpenFileDialog();
+
+            if (dialog.ShowDialog() == CommonFileDialogResult.Ok)
+            {
+                linesInputTextBox.Lines = GetInputFromScript(dialog.FileName);
+            }
+        }
+        static void UpdateSettingsVars()
+        {
+            scriptsFolder = scriptsFolderTextBox.Text;
+            keyToBind = keyToBindTextBox.Text;
+            waitTime = waitTimeInput.Value.ToString();
+        }
+        static string[] GenerateScriptLines(string[] lines)
+        {
+            int setLineCount;
+            if (lines.Length % 5 == 0)
+                setLineCount = lines.Length / 5;
+            else
+                setLineCount = (int)Math.Ceiling((double)lines.Length / 5.0);
+
+            string[] scriptLines = new string[lines.Length + setLineCount +1];
+
+            scriptLines[0] = "bind " + keyToBind + " \"spamSet0\"";
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                scriptLines[i + 1] = "alias spam" + (i) + " \"say " + lines[i].Replace("\"", "'") + "\"";
+            }
+
+            for (int i = 0; i < setLineCount; i++)
+            {
+                string newLine = "alias spamSet" + i + " \"";
+                for (int o = 0; o < 5; o++)
+                {
+                    newLine += "spam" + ((i * 5) + o) + "; wait " + waitTime + "; ";
+
+                    if ((i * 5) + o >= lines.Length - 1)
+                        break;
+                }
+                if (i != setLineCount - 1)
+                    newLine += "spamSet" + (i + 1) + ";";
+                newLine += "\"";
+
+                scriptLines[lines.Length + 1 + i] = newLine;
+            }
+            return scriptLines;
+        }
+        static string[] GetInputFromScript(string scriptFilePath)
+        {
+            string[] scriptLines = File.ReadAllLines(scriptFilePath);
+
+            int lineCount = scriptLines.Length - 2;
+            lineCount = (int)((float)lineCount * (5f / 6f)); 
+            //^^this literally CANNOT be a good way to do this but it works
+
+            string[] lines = new string[lineCount];
+
+            for(int i = 0; i < lineCount; i++)
+            {
+                lines[i] = scriptLines[i + 2].Split('"')[1].Remove(0, 4);
+            }
+            return lines;
+        }
+    }
+}
